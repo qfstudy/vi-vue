924ddc1487a51392d35d6b117cbe63fa6d7cd0f0
qfstudy <qifeistudy@gmail.com>
2018-12-10 20:05:45 +0800

    grid col按比例显示

--- src/packages/grid/src/col.vue
+++ src/packages/grid/src/col.vue
@@ -1,19 +1,52 @@
 <template>
-    <div class="vi-col">
+    <div class="vi-col" :class="colClass" :style="colStyle">
         <slot></slot>
     </div>
 </template>
 
 <script>
 export default {
-    name:'ViCol'
+    name:'ViCol',
+    data(){
+        return{
+            gutter:0
+        }
+    },
+    props:{
+        span:{
+            type:[Number]
+        }
+    },
+    computed:{
+        colClass(){
+            // console.log(55)
+            let{span}=this
+            // console.log(span)
+            return[
+                span&&`vi-col-${span}`
+            ]
+        },
+        colStyle(){
+            return{
+                paddingLeft:this.gutter/2 + 'px',
+                paddingRight:this.gutter/2 + 'px'
+            }
+        }
+    },
+    
 }
 </script>
 
 <style lang="scss" scoped>
+    $class-prefix: vi-col-;
     .vi-col{
-        width: 50%;
-        // background: red;
-        border: 1px solid green;
+        // width: 100%;
+        // display: flex;
+        @for $n from 1 through 24{
+            &.#{$class-prefix}#{$n}{
+                width: ($n/24) * 100%;
+            }
+        }
+       
     }
 </style>
--- src/packages/grid/src/row.vue
+++ src/packages/grid/src/row.vue
@@ -1,17 +1,40 @@
 <template>
-    <div class="vi-row">
+    <div class="vi-row" :style="rowStyle">
         <slot></slot>
     </div>
 </template>
 
 <script>
 export default {
-    name:'ViRow'
+    name:'ViRow',
+    props:{
+        gutter:{
+            type:[Number,String]
+        }
+    },
+    computed:{
+        rowStyle(){
+            let{gutter}=this
+            return {
+                marginLeft:-gutter/2+'px',
+                marginRight:-gutter/2+'px'
+            }
+        },
+    },
+    mounted(){
+        //    console.log(this.$children)
+        this.$children.forEach((vm)=>{
+            vm.gutter=this.gutter
+            // console.log(vm.gutter)
+        })
+    }
 }
 </script>
 
 <style lang="scss" scoped>
     .vi-row{
         display: flex;
+        // width:100%;
+        // height: 100%;
     }
 </style>
