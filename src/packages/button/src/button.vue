40f683d09143fc0972ea10df42087d6246f70cdc
qfstudy <qifeistudy@gmail.com>
2019-01-03 20:26:00 +0800

    调整button.vue的代码以及样式

--- src/packages/button/src/button.vue
+++ src/packages/button/src/button.vue
@@ -1,10 +1,12 @@
 <template>
-    <button @click="handleClick" class="vi-button" :disabled=disabled :class="[type&&'vi-button-'+type,iconPosition&&'vi-button-'+iconPosition,circle&&'vi-button-'+circle,disabled&&'vi-button-'+disabled]">
-        <span v-if="iconName" class="vi-button-icon">
-            <vi-icon :viIconName="iconName" :viIconSize="iconSize"></vi-icon>
-        </span>
-        <span class="vi-button-content">
-            <slot></slot>
+    <button @click="handleClick" class="vi-button" :disabled=disabled :class=buttonClass>
+        <span class="vi-button-wrapper" :class=wrapperClass>
+            <span v-if="iconName" class="vi-button-icon">
+                <vi-icon :viIconName="iconName" :viIconSize="iconSize"></vi-icon>
+            </span>
+            <span class="vi-button-content">
+                <slot></slot>
+            </span>
         </span>
     </button>
 </template>
@@ -40,7 +42,7 @@ export default {
         },
         iconPosition:{
             type: String,
-            // default: 'left',
+            default: 'left',
             validator(value){
                 return[
                     'left',
@@ -60,6 +62,20 @@ export default {
             this.$emit('click', event);
         }
     },
+    computed:{
+        buttonClass(){
+            return {
+                [`vi-button-${this.type}`]:this.type,
+                [`vi-button-${this.disabled}`]:this.disabled,
+                [`vi-button-${this.circle}`]:this.circle
+            }
+        },
+        wrapperClass(){
+            return {
+                [`vi-button-${this.iconPosition}`]:this.iconName&&this.iconPosition
+            }
+        }
+    }
 }
 </script>
 
@@ -72,15 +88,40 @@ export default {
         cursor: pointer;
         border: 1px solid #e1e1e1;
         white-space: nowrap;
-        display: inline-flex;
-        align-items: center;
-        vertical-align: middle;
-        .vi-button-icon>*{
+        
+        .vi-button-wrapper{
+            display: inline-flex;
+            align-items: center;
             vertical-align: middle;
+            .vi-button-content{
+                padding: 0.5em 0.8em; 
+            }
+            .vi-button-icon>*{
+                vertical-align: middle;
+            }
+            &.vi-button-right{
+                .vi-button-icon{
+                    order: 2;
+                }
+                .vi-button-content{
+                    order: 1;
+                    padding-right: 0.2em;
+                    padding-left: 0.2em;
+                }
+            }
+            &.vi-button-left{
+                .vi-button-icon{
+                    order: 1;
+                }
+                .vi-button-content{
+                    order: 2;
+                    padding-right: 0.2em;
+                    padding-left: 0.2em;
+                }
+            }
+            
         }
-        .vi-button-content{
-            padding: 0.6em ; 
-        }
+        
         &.vi-button-primary{
             background-color: #2d8cf0;
             border-color: #2d8cf0;
@@ -109,28 +150,14 @@ export default {
         &:hover{
             opacity: 0.8;
         }
-        &.vi-button-right{
-            > .vi-button-icon{
-                order: 2;
-            }
-            > .vi-button-content{
-                order: 1;
-            }
-        }
-        &.vi-button-left{
-            > .vi-button-icon{
-                order: 1;
-            }
-            > .vi-button-content{
-                order: 2;
-            }
-        }
         &.vi-button-circle{
-            .vi-button-content{
+            .vi-button-wrapper{
                 padding: 0; 
+                .vi-button-content{
+                    padding: 0;
+                }
             }
-            padding: 0;
-            margin: 0;
+            display: inline-block;
             border-radius: 50%;
         }
         &.vi-button-disabled{
